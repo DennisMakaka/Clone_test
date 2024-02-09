20b027353d1713e092f46487762938f09c86cf7c
felabel <felicityabel2016@gmail.com>
2024-02-09 06:37:44 +0100

    created models

--- models/base_model.py
+++ models/base_model.py
@@ -29,45 +29,33 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-    """Save the state of the object to the data store.
+        """Save the state of the object to the data store.
 
-    This method updates the object's ``updated_at`` attribute with the current
-    date and time, and then saves the object to the data store by calling the
-    ``save`` method of the ``Storage`` class.
+        This method updates the object's ``updated_at`` attribute with the current
+        date and time, and then saves the object to the data store by calling the
+        ``save`` method of the ``Storage`` class.
 
-    Returns:
-        None
-    """
-    self.updated_at = datetime.today()
-    models.storage.save()
+        Returns:
+            None
+        """
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        def to_dict(self):
-            """Return the dictionary of the BaseModel instance.
-        
-            Includes the key/value pair __class__ representing
-            the class name of the object.
-            """
-            rdict = self.__dict__.copy()
-            rdict["created_at"] = self.created_at.isoformat()
-            rdict["updated_at"] = self.updated_at.isoformat()
-            rdict["__class__"] = self.__class__.__name__
-            return rdict
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
         return rdict
+    
 
     def __str__(self):
-        def __str__(self):
-                """Return the print/str representation of the BaseModel instance.
-        
-                Returns:
-                    str: The print/str representation of the BaseModel instance.
-                """
-                clname = self.__class__.__name__
-                return f"[{clname}] ({self.id}) {self.__dict__}"
+        """Return the print/str representation of the BaseModel instance.
+
+        Returns:
+            str: The print/str representation of the BaseModel instance.
+        """
         clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        return f"[{clname}] ({self.id}) {self.__dict__}"
+       
 
