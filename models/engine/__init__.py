a431f29abb179fe82564ead39e265fd13ff71ec4
felabel <felicityabel2016@gmail.com>
2024-02-11 06:53:36 +0100

    removing files

--- models/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-from models.engine.file_storage import FileStorage
-
-# Create a unique FileStorage instance for your application
-storage = FileStorage()
-
-# Load serialized data from the JSON file into memory
-storage.reload()
-
